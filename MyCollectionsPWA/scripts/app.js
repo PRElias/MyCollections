dc4bbebc4d1a9f3e651e91d1ec6550c67fd0e610
Pepeu <ppuspfc@gmail.com>
2018-01-27 00:36:51 -0200

    ajuses no app, ícones e javascript

--- MyCollectionsPWA/scripts/app.js
+++ MyCollectionsPWA/scripts/app.js
@@ -1,92 +1,74 @@
-console.log("arquivo carregado");
+//document.getElementById('butRefresh').addEventListener('click', function () {
+//    console.log("botão clicado");
+//});
 
-document.getElementById('butRefresh').addEventListener('click', function () {
-    console.log("botão clicado");
-    app.getGames();
-});
+//localStorage.clear();
 
-//(function () {
-//'use strict';
 
 var app = {
     isLoading: true,
     visibleCards: {},
-    selectedGames: [],
+    games: [],
     spinner: document.querySelector('.loader'),
     cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('.main')
 };
 
-document.addEventListener("DOMContentLoaded", function () {
+//document.addEventListener("DOMContentLoaded", function () {
 
-});
+//});
+
 
 app.getGames = function () {
-    var game;
-    var items = [];
-    var url = 'http://mycollectionsapi.paulorobertoelias.com.br/api/Games';
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
+    //debugger;
+    app.spinner.setAttribute('hidden', false);
+    app.games = localStorage.getItem("games");
+    if (app.games === undefined || app.games === null) {
 
-                var response = JSON.parse(request.response);
-                for (var index in response) {
-                    var game = response[index];
-
-                    items.push(
-                        "<div class='card cardTemplate' id='" + game.gameID + "'><p>" + game.name + "</p>" +
-                        "<img class='cover' src='" + game.cover + "' alt='logo' /img>" +
-                        "</div > "
-                    );
+        var url = 'http://mycollectionsapi.paulorobertoelias.com.br/api/Games';
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    app.games = request.response;
+                    localStorage.setItem("games", app.games);
                 }
-
-                var wrapper = document.createElement('div');
-                wrapper.innerHTML = items.join("");
-
-                var main = document.getElementById("main_div");
-                main.appendChild(wrapper);
-                
-                app.spinner.setAttribute('hidden', true);
             }
         };
-    };
-    request.open('GET', url);
-    request.send();
+        request.open('GET', url, false);
+        request.send();
+    }
 
-    // TODO add saveSelectedGames function here
-    // Save list of cities to localStorage.
-    app.saveSelectedGames = function () {
-        var selectedGames = JSON.stringify(app.selectedGames);
-        localStorage.selectedGames = selectedGames;
-    };
+    app.renderizeGames();
+    app.spinner.setAttribute('hidden', true);
+    
 };
 
+app.renderizeGames = function () {
+    //debugger
+    var items = [];
+    var games = JSON.parse(app.games);
+    for (var index in games) {
+        var game = games[index];
 
+        items.push(
+            "<div class='card cardTemplate' id='" + game.gameID + "'><p>" + game.name + "</p>" +
+            "<img class='cover' src='" + game.cover + "' alt='logo' /img>" +
+            "</div > "
+        );
+    }
 
-app.selectedGames = localStorage.selectedGames;
-if (app.selectedGames) {
-    app.selectedGames = JSON.parse(app.selectedGames);
-    app.selectedGames.forEach(function (city) {
-        //app.getGames();
-    });
-} else {
-    /* The user is using the app for the first time, or the user has not
-     * saved any cities, so show the user some fake data. A real app in this
-     * scenario could guess the user's location via IP lookup and then inject
-     * that data into the page.
-     */
-    //app.updateForecastCard(initialWeatherForecast);
-    //app.selectedCities = [
-    //    { key: initialWeatherForecast.key, label: initialWeatherForecast.label }
-    //];
-    //app.saveSelectedCities();
-}
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = items.join("");
 
-// TODO add service worker code here
+    var main = document.getElementById("main_div");
+    main.appendChild(wrapper);
+};
+
+app.getGames();
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('./service-worker.js')
         .then(function () { console.log('Service Worker Registered'); });
 }
-//});
--- MyCollectionsPWA/service-worker.js
+++ MyCollectionsPWA/service-worker.js
@@ -1,23 +1,11 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 var dataCacheName = 'gameData-v1';
 var cacheName = 'gamePWA-final-1';
 var filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
   '/styles/inline.css'
 ];
 
@@ -56,35 +44,35 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'http://localhost:8091/Api/Games';
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    /*
-     * When the request URL contains dataUrl, the app is asking for fresh
-     * weather data. In this case, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
-    e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
-      })
-    );
-  } else {
-    /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
-});
+//self.addEventListener('fetch', function(e) {
+//  console.log('[Service Worker] Fetch', e.request.url);
+//  var dataUrl = 'http://mycollectionsapi.paulorobertoelias.com.br/api/Games';
+//  if (e.request.url.indexOf(dataUrl) > -1) {
+//    /*
+//     * When the request URL contains dataUrl, the app is asking for fresh
+//     * weather data. In this case, the service worker always goes to the
+//     * network and then caches the response. This is called the "Cache then
+//     * network" strategy:
+//     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+//     */
+//    e.respondWith(
+//      caches.open(dataCacheName).then(function(cache) {
+//        return fetch(e.request).then(function(response){
+//          cache.put(e.request.url, response.clone());
+//          return response;
+//        });
+//      })
+//    );
+//  } else {
+//    /*
+//     * The app is asking for app shell files. In this scenario the app uses the
+//     * "Cache, falling back to the network" offline strategy:
+//     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+//     */
+//    e.respondWith(
+//      caches.match(e.request).then(function(response) {
+//        return response || fetch(e.request);
+//      })
+//    );
+//  }
+//});
