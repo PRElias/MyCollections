4ce1c248f2b8435b251f0d356dfc7f58faec380d
PRElias <ppuspfc@gmail.com>
2018-08-14 13:34:55 -0300

    Games CRUD

---
+++ MyCollections/Controllers/GamesApiController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyCollections.Models;
+using MyCollections.Services;
+
+namespace MyCollections.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Games")]
+    public class GamesApiController : Controller
+    {
+        private readonly MyCollectionsContext _context;
+
+        public GamesApiController(MyCollectionsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Games
+        [HttpGet]
+        public IEnumerable<GamesDistinctView> GetGames()
+        {
+            //return _context.Game.Include("Store").Include("System").Where(a => a.Active == true).OrderBy(n => n.Name);
+            return _context.GamesDistinctView.ToList();
+        }
+
+        // GET: api/GetDistinctGames
+        //[HttpGet("GetDistinctGames")]
+        //public IEnumerable<dynamic> GetDistinctGames()
+        //{
+        //    var gameDistinct = from recordset in _context.Game
+        //                       where recordset.Active == true
+        //                       orderby recordset.Name
+        //                       select new
+        //                       {
+        //                           recordset.Name,
+        //                           recordset.Cover
+        //                       };
+        //    return gameDistinct;
+        //}
+
+
+        // GET: api/Games/5
+        [HttpGet("{name}")]
+        public IEnumerable<dynamic> GetGame([FromRoute] string name)
+        {
+            // var game = from g in _context.Game
+            //            join s in _context.System on g.SystemID equals s.SystemID
+            //            let systemName = s.Name
+            //            where g.Name == name
+            //            where g.Active == true
+            //            orderby g.Name
+            //            select new
+            //            {
+            //                g.Name,
+            //                g.Cover,
+            //                systemName
+            //            };
+            // return game;
+            return _context.GamesDetailsView.Where(n => n.Game == name).ToList();
+        }
+
+        [HttpGet("GetFromSteam")]
+        public async Task<dynamic> GetFromSteam()
+        {
+            string key = _context.Param.FirstOrDefault(p => p.key == "steam-key").value;
+            string steamid = _context.Param.FirstOrDefault(p => p.key == "steam-steamid").value;
+            int gameNewCount = 0;
+            int gameUpdateCount = 0;
+
+            if (key == string.Empty || steamid == string.Empty)
+            {
+                return StatusCode(204);
+            }
+
+            var games = await Steam.GetFromSteam(key, steamid);
+
+            if (games != null)
+            {
+                foreach (var item in games.response.games)
+                {
+                    if (_context.Game.Any(g => g.SteamApID == item.appid))
+                    {
+                        var existingGame = _context.Game.FirstOrDefault(i => i.SteamApID == item.appid);
+                        gameUpdateCount++;
+                        existingGame.PlayedTime = item.playtime_forever;
+                        _context.Game.Update(existingGame);
+                        _context.SaveChanges();
+                        continue;
+                    }
+
+                    Game game = new Game();
+                    gameNewCount++;
+                    game.Name = item.name;
+                    game.SteamApID = item.appid;
+                    game.PlayedTime = item.playtime_forever;
+                    if (item.img_logo_url != "" && item.img_logo_url != null)
+                    {
+                        game.Logo = "http://media.steampowered.com/steamcommunity/public/images/apps/" + item.appid + "/" + item.img_logo_url + ".jpg";
+                    }
+                    if (item.img_icon_url != "" && item.img_icon_url != null)
+                    {
+                        game.Cover = "http://media.steampowered.com/steamcommunity/public/images/apps/" + item.appid + "/" + item.img_icon_url + ".jpg";
+                    }
+                    game.StoreID = _context.Store.FirstOrDefault(s => s.Name == "Steam").StoreID;
+                    game.SystemID = _context.System.FirstOrDefault(s => s.Name == "PC").SystemID;
+                    game.Active = true;
+                    _context.Game.Add(game);
+                    _context.SaveChanges();
+                }
+            }
+
+            return Ok("Importado com sucesso. Jogos novos: " + gameNewCount.ToString() + ", jogos atualizados: " + gameUpdateCount.ToString());
+        }
+    }
+}
--- MyCollections/Controllers/GamesController.cs
+++ MyCollections/Controllers/GamesController.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyCollections.Models;
-using MyCollections.Services;
 
 namespace MyCollections.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Games")]
     public class GamesController : Controller
     {
         private readonly MyCollectionsContext _context;
@@ -19,101 +18,148 @@ namespace MyCollections.Controllers
             _context = context;
         }
 
-        // GET: api/Games
-        [HttpGet]
-        public IEnumerable<GamesDistinctView> GetGames()
+        // GET: Games
+        public async Task<IActionResult> Index()
         {
-            //return _context.Game.Include("Store").Include("System").Where(a => a.Active == true).OrderBy(n => n.Name);
-            return _context.GamesDistinctView.ToList();
+            var myCollectionsContext = _context.Game.Include(g => g.Store).Include(g => g.System);
+            return View(await myCollectionsContext.ToListAsync());
         }
 
-        // GET: api/GetDistinctGames
-        //[HttpGet("GetDistinctGames")]
-        //public IEnumerable<dynamic> GetDistinctGames()
-        //{
-        //    var gameDistinct = from recordset in _context.Game
-        //                       where recordset.Active == true
-        //                       orderby recordset.Name
-        //                       select new
-        //                       {
-        //                           recordset.Name,
-        //                           recordset.Cover
-        //                       };
-        //    return gameDistinct;
-        //}
-
-
-        // GET: api/Games/5
-        [HttpGet("{name}")]
-        public IEnumerable<dynamic> GetGame([FromRoute] string name)
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            // var game = from g in _context.Game
-            //            join s in _context.System on g.SystemID equals s.SystemID
-            //            let systemName = s.Name
-            //            where g.Name == name
-            //            where g.Active == true
-            //            orderby g.Name
-            //            select new
-            //            {
-            //                g.Name,
-            //                g.Cover,
-            //                systemName
-            //            };
-            // return game;
-            return _context.GamesDetailsView.Where(n => n.Game == name).ToList();
-        }
-
-        [HttpGet("GetFromSteam")]
-        public async Task<dynamic> GetFromSteam()
-        {
-            string key = _context.Param.FirstOrDefault(p => p.key == "steam-key").value;
-            string steamid = _context.Param.FirstOrDefault(p => p.key == "steam-steamid").value;
-            int gameNewCount = 0;
-            int gameUpdateCount = 0;
-
-            if (key == string.Empty || steamid == string.Empty)
+            if (id == null)
             {
-                return StatusCode(204);
+                return NotFound();
             }
 
-            var games = await Steam.GetFromSteam(key, steamid);
-
-            if (games != null)
+            var game = await _context.Game
+                .Include(g => g.Store)
+                .Include(g => g.System)
+                .FirstOrDefaultAsync(m => m.GameID == id);
+            if (game == null)
             {
-                foreach (var item in games.response.games)
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            ViewData["StoreID"] = new SelectList(_context.Store, "StoreID", "Name");
+            ViewData["SystemID"] = new SelectList(_context.System, "SystemID", "Name");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GameID,Name,FriendlyName,Cover,Logo,SystemID,StoreID,BuyDate,Price,PlayedTime,Purchased,SteamApID,Active")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreID"] = new SelectList(_context.Store, "StoreID", "Name", game.StoreID);
+            ViewData["SystemID"] = new SelectList(_context.System, "SystemID", "Name", game.SystemID);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            ViewData["StoreID"] = new SelectList(_context.Store, "StoreID", "Name", game.StoreID);
+            ViewData["SystemID"] = new SelectList(_context.System, "SystemID", "Name", game.SystemID);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GameID,Name,FriendlyName,Cover,Logo,SystemID,StoreID,BuyDate,Price,PlayedTime,Purchased,SteamApID,Active")] Game game)
+        {
+            if (id != game.GameID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
                 {
-                    if (_context.Game.Any(g => g.SteamApID == item.appid))
-                    {
-                        var existingGame = _context.Game.FirstOrDefault(i => i.SteamApID == item.appid);
-                        gameUpdateCount++;
-                        existingGame.PlayedTime = item.playtime_forever;
-                        _context.Game.Update(existingGame);
-                        _context.SaveChanges();
-                        continue;
-                    }
-
-                    Game game = new Game();
-                    gameNewCount++;
-                    game.Name = item.name;
-                    game.SteamApID = item.appid;
-                    game.PlayedTime = item.playtime_forever;
-                    if (item.img_logo_url != "" && item.img_logo_url != null)
-                    {
-                        game.Logo = "http://media.steampowered.com/steamcommunity/public/images/apps/" + item.appid + "/" + item.img_logo_url + ".jpg";
-                    }
-                    if (item.img_icon_url != "" && item.img_icon_url != null)
-                    {
-                        game.Cover = "http://media.steampowered.com/steamcommunity/public/images/apps/" + item.appid + "/" + item.img_icon_url + ".jpg";
-                    }
-                    game.StoreID = _context.Store.FirstOrDefault(s => s.Name == "Steam").StoreID;
-                    game.SystemID = _context.System.FirstOrDefault(s => s.Name == "PC").SystemID;
-                    game.Active = true;
-                    _context.Game.Add(game);
-                    _context.SaveChanges();
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
                 }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.GameID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StoreID"] = new SelectList(_context.Store, "StoreID", "Name", game.StoreID);
+            ViewData["SystemID"] = new SelectList(_context.System, "SystemID", "Name", game.SystemID);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
             }
 
-            return Ok("Importado com sucesso. Jogos novos: " + gameNewCount.ToString() + ", jogos atualizados: " + gameUpdateCount.ToString());
+            var game = await _context.Game
+                .Include(g => g.Store)
+                .Include(g => g.System)
+                .FirstOrDefaultAsync(m => m.GameID == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var game = await _context.Game.FindAsync(id);
+            _context.Game.Remove(game);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.GameID == id);
         }
     }
 }
