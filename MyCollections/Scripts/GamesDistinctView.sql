bf678b3d8720b71000cdf4e8042122d49ca4cf9b
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-08-15 22:32:23 -0300

    View fix

--- MyCollections/Scripts/GamesDistinctView.sql
+++ MyCollections/Scripts/GamesDistinctView.sql
@@ -1,8 +1,8 @@
 ﻿ALTER VIEW GamesDistinct AS
 SELECT
 DISTINCT
-T1.[FriendlyName] AS Name
+COALESCE(T1.[FriendlyName], T1.[Name]) AS Name
 ,(SELECT TOP 1 T2.[Cover] FROM [DB_A0A084_mycollection].[dbo].[Game] T2 WHERE T2.[FriendlyName] = T1.[FriendlyName] AND T2.[Cover] IS NOT NULL) AS Cover
 ,(SELECT COUNT(T2.[GameID]) FROM [DB_A0A084_mycollection].[dbo].[Game] T2 WHERE T2.[FriendlyName] = T1.[FriendlyName] AND T2.[Active] = 1) AS Quantity
 FROM [DB_A0A084_mycollection].[dbo].[Game] T1
-WHERE [Active] = 1
+WHERE T1.[Active] = 1
