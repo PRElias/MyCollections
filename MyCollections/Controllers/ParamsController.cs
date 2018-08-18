6e4b7e3a81a1ac2226dfd228ba7b29dde7235da9
Pepeu <ppuspfc@gmail.com>
2018-08-18 11:09:28 -0300

    Tela de params

---
+++ MyCollections/Controllers/ParamsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyCollections.Models;
+
+namespace MyCollections.Controllers
+{
+    public class ParamsController : Controller
+    {
+        private readonly MyCollectionsContext _context;
+
+        public ParamsController(MyCollectionsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Params
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Param.ToListAsync());
+        }
+
+        // GET: Params/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @param = await _context.Param.FindAsync(id);
+            if (@param == null)
+            {
+                return NotFound();
+            }
+            return View(@param);
+        }
+
+        // POST: Params/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("key,value")] Param @param)
+        {
+            if (id != @param.key)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@param);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (_context.Param.Find(@param.key) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@param);
+        }
+
+    }
+}
