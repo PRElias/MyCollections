1d668ef7f441bacd3eadf03d67d543321ea22036
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-10-09 20:27:22 -0300

    Ocultando telas administrativas

--- MyCollections/Controllers/ParamsController.cs
+++ MyCollections/Controllers/ParamsController.cs
@@ -10,69 +10,69 @@ namespace MyCollections.Controllers
     [Authorize]
     public class ParamsController : Controller
     {
-        private readonly MyCollectionsContext _context;
+    //    private readonly MyCollectionsContext _context;
 
-        public ParamsController(MyCollectionsContext context)
-        {
-            _context = context;
-        }
+    //    public ParamsController(MyCollectionsContext context)
+    //    {
+    //        _context = context;
+    //    }
 
-        // GET: Params
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Param.ToListAsync());
-        }
+    //    // GET: Params
+    //    public async Task<IActionResult> Index()
+    //    {
+    //        return View(await _context.Param.ToListAsync());
+    //    }
 
-        // GET: Params/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+    //    // GET: Params/Edit/5
+    //    public async Task<IActionResult> Edit(string id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            var @param = await _context.Param.FindAsync(id);
-            if (@param == null)
-            {
-                return NotFound();
-            }
-            return View(@param);
-        }
+    //        var @param = await _context.Param.FindAsync(id);
+    //        if (@param == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        return View(@param);
+    //    }
 
-        // POST: Params/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("key,value")] Param @param)
-        {
-            if (id != @param.key)
-            {
-                return NotFound();
-            }
+    //    // POST: Params/Edit/5
+    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Edit(string id, [Bind("key,value")] Param @param)
+    //    {
+    //        if (id != @param.key)
+    //        {
+    //            return NotFound();
+    //        }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(@param);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (_context.Param.Find(@param.key) == null)
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@param);
-        }
+    //        if (ModelState.IsValid)
+    //        {
+    //            try
+    //            {
+    //                _context.Update(@param);
+    //                await _context.SaveChangesAsync();
+    //            }
+    //            catch (DbUpdateConcurrencyException)
+    //            {
+    //                if (_context.Param.Find(@param.key) == null)
+    //                {
+    //                    return NotFound();
+    //                }
+    //                else
+    //                {
+    //                    throw;
+    //                }
+    //            }
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        return View(@param);
+    //    }
 
-    }
+    //}
 }
