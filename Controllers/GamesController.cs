8142f9af3a11aeeb6c7257e2aea22bd22ea17351
PRElias <ppuspfc@gmail.com>
2020-05-15 10:33:43 -0300

    lendo json

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using MyCollections.Models;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace MyCollections.Controllers
 {
     public class GamesController : Controller
     {
+        private System.Collections.Generic.List<Game> games = new System.Collections.Generic.List<Game>();
         public IActionResult Index()
         {
-            return View();
+            LoadJson();
+            return View(games);
+        }
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("docs/games/games.json"))
+            {
+                string json = r.ReadToEnd();
+                games = JsonConvert.DeserializeObject<System.Collections.Generic.List<Game>>(json);
+            }
         }
     }
 }
