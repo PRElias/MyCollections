b98b2d57a485c0aba36747704164d52cbb904565
PRElias <ppuspfc@gmail.com>
2020-05-18 22:09:28 -0300

    if para importScripts

--- docs/pwabuilder-sw.js
+++ docs/pwabuilder-sw.js
@@ -1,22 +1,28 @@
 'use strict';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE_NAME = 'static-cache';
+const OFFLINE_URL = '/index.html';
 
-if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉 for service-work.js`);
-} else {
-    console.log(`Boo! Workbox didn't load 😬 for service-work.js`);
-}
+if ('undefined' === typeof window) {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-workbox.routing.registerRoute(
+
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
+
+  workbox.routing.registerRoute(
     /.*\.(?:png|jpg|jpeg|svg|gif|js|css|woff|woff2|html)/g,
     new workbox.strategies.CacheFirst({
-        cacheName: "static-cache",
-        cacheableResponse: {
-            statuses: [0, 200]
-        }
+      cacheName: CACHE_NAME,
+      cacheableResponse: {
+        statuses: [0, 200]
+      }
     })
-);
+  );
+}
 
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
