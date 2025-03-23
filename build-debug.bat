16dbb7eb5aab2c8cd5fafb1011ccc77b1c7301ac
Paulo Roberto Elias <ppuspfc@gmail.com>
2025-03-22 22:02:55 -0300

    mais capas e fix splashscreen

--- build-debug.bat
+++ build-debug.bat
@@ -1,5 +1,5 @@
 del /f /q .\platforms\android\app\build\outputs\bundle\release\*
 cd AppCordova
 wsl.exe docker run -v .:/opt/src --rm androidbuild cordova build android --debug 
-ren .\platforms\android\app\build\outputs\bundle\release\app-debug mycollections.apk
+ren .\platforms\android\app\build\outputs\bundle\release\app-debug.apk mycollections.apk
 cmd.exe
